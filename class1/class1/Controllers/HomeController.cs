19cc86d7d6c0d17e79543a7e20dccd6580071404
pukarsai <85217415+pukarsai@users.noreply.github.com>
2022-02-22 17:09:53 +0545

    newone

---
+++ class1/class1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Class1.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace class1.Controllers
+{
+    public class HomeController : Controller
+    {
+        todolistEntities obj = new todolistEntities();
+        public ActionResult Index()
+        {
+            var lst = obj.activityTbls.ToList();
+           
+            ViewBag.Activity = lst;
+
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
